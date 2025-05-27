bbc462fe3af40a37d8a12114edfa1225af10d0a8
Jasmine-Abucejo <abucejojasmine@gmail.com>
2025-05-27 15:39:18 +0800

    add deployment scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import bookRoutes from "./routers/bookRoutes.js";
 
@@ -8,6 +9,15 @@ const app = express();
 app.use(express.json());
 app.use("/api/books", bookRoutes);
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
