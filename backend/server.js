a933f94ec3bcd03d9c0b03a903f07f79b9812e15
Jasmine-Abucejo <abucejojasmine@gmail.com>
2025-07-07 18:55:06 +0800

    fix server script

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   connectDB();
